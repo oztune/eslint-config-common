951f7bda23a7e62c457b629040f9473d33c4b4db
Oz Michaeli <oz@appfigures.com>
2016-06-26 12:21:57 -0400

    Update to eslint 2.0, and added some useful rules like jsx-no-undef. Tried to use the string 'error' instead of 2 in the rules, but sublime's linter ignored it.

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 module.exports = {
-    "rules": {
-        "indent": [
-            2,
-            "tab"
-        ],
-        "quotes": [
-            2,
-            "single"
-        ],
-        "linebreak-style": [
-            2,
-            "unix"
-        ],
-        "semi": [
-            2,
-            "never"
-        ],
-        "no-unused-vars": [
-            2,
-            {
-                "vars": "all",
-                "args": "none"
-            }
-        ],
-        "react/jsx-uses-react": 1
-    },
-    "env": {
-        "es6": true,
-        "node": true,
-        "browser": true
-    },
-    "extends": "eslint:recommended",
-    "ecmaFeatures": {
-        "jsx": true,
-        "modules": true,
-        "experimentalObjectRestSpread": true
-    },
-    "parser": "babel-eslint",
-    "plugins": [
-        "react"
-    ]
+	"extends": "eslint:recommended",
+	"env": {
+		"es6": true,
+		"node": true,
+		"browser": true
+	},
+	"parserOptions": {
+		"ecmaVersion": 7,
+		"sourceType": "module",
+		"ecmaFeatures": {
+			"impliedStrict": true,
+			"jsx": true,
+			"experimentalObjectRestSpread": true
+		}
+	},
+	"settings": {
+		"react": {
+			"pragma": "React",
+			"version": "15.0"
+		}
+	},
+	"parser": "babel-eslint",	
+	"plugins": ["react"],
+	"rules": {
+		"indent": [2, "tab"],
+		"quotes": [2, "single"],
+		"linebreak-style": [2, "unix"],
+		"semi": [2, "never"],
+		"no-unused-vars": [2,
+			{
+				"vars": "all",
+				"args": "none"
+			}
+		],
+		"react/jsx-uses-react": 2,
+		"react/jsx-no-undef": 2,
+		"react/react-in-jsx-scope": 2,
+		"react/no-deprecated": 2,
+		"react/no-direct-mutation-state": 2,
+		"react/jsx-no-duplicate-props": 2
+	}
 }
