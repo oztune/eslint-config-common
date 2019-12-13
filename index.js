0e52cdef3e4c8793078d43355002a0a36055ee29
Oz <oztune@users.noreply.github.com>
2019-12-13 11:58:32 -0500

    Making eslint work well with prettier

--- index.js
+++ index.js
@@ -1,73 +1,8 @@
 module.exports = {
-	"extends": "eslint:recommended",
-	// This is inspired by:
-	// - https://github.com/babel/babel-eslint/issues/663#issuecomment-519855344
-	// - https://github.com/facebook/create-react-app/blob/master/packages/eslint-config-react-app/index.js#L55
-	overrides: [
-		{
-			files: ["**/*.ts", "**/*.tsx"],
-			parser: "@typescript-eslint/parser",
-			parserOptions: {
-				ecmaVersion: 2018,
-				sourceType: 'module',
-				ecmaFeatures: {
-				  jsx: true,
-				},
-				// typescript-eslint specific options
-				warnOnUnsupportedTypeScriptVersion: true,
-			},
-			plugins: ["@typescript-eslint"],
-			// If adding a typescript-eslint version of an existing ESLint rule,
-			// make sure to disable the ESLint rule here.
-			rules: {
-				'default-case': 'off',
-				'no-dupe-class-members': 'off',
-				'no-undef': 'off',
-				'no-unused-vars': 'off'
-			}
-		}
-	],
-	"env": {
-		"es6": true,
-		"node": true,
-		"browser": true
-	},
-	"parserOptions": {
-		"ecmaVersion": 7,
-		"sourceType": "module",
-		"ecmaFeatures": {
-			"impliedStrict": true,
-			"jsx": true
-		}
-	},
-	"settings": {
-		"react": {
-			"pragma": "React",
-			"version": "16.0"
-		}
-	},
-	"parser": "babel-eslint",	
-	"plugins": ["react", "react-hooks"],
-	"rules": {
-		"indent": ["error", "tab"],
-		"quotes": ["error", "single"],
-		"linebreak-style": ["error", "unix"],
-		"semi": ["error", "never"],
-		"no-unused-vars": ["warn",
-			{
-				"vars": "all",
-				"args": "none"
-			}
-		],
-		"react/jsx-uses-react": "error",
-		"react/jsx-uses-vars": "error",
-		"react/jsx-no-undef": "error",
-		"react/react-in-jsx-scope": "error",
-		"react/no-deprecated": "error",
-		"react/no-direct-mutation-state": "error",
-		"react/jsx-no-duplicate-props": "error",
-		"react/require-render-return": "warn",
-		"react-hooks/rules-of-hooks": "error",
-    	"react-hooks/exhaustive-deps": "warn"
-	}
+	// We let prettier override rules that conflict with its config.
+	// Ref: https://github.com/prettier/eslint-config-prettier#installation
+	//
+	// We need to put the real config in ./base so that prettier
+	// configs can override it.
+	"extends": ["./base", "prettier", "prettier/@typescript-eslint", "prettier/babel", "prettier/react"]
 }
